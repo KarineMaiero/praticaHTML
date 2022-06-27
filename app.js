d4de041c89466868c80c54376bb3710906706b91
karinemaiero <karine_gino@yahoo.com.br>
2022-06-27 18:55:24 -0300

    jutando js - parte 3 concluída

---
+++ app.js
@@ -0,0 +1,17 @@
+let tarefas1 = `Ir ao mercado`
+let tarefas2 = `Estudar programação`
+let tarefas3 = `Atualizar LinkedIn`
+
+console.log(`Minhas tarefas são: ${tarefas1}, ${tarefas2} e ${tarefas3}`)
+
+let tarefas = []
+
+tarefas.push(tarefas1, tarefas2, tarefas3)
+
+console.log(tarefas)
+
+let novaTarefa = `Regar plantas`
+
+alert(`Sua nova tarefa é: ${novaTarefa}`)
+
+tarefas.push(novaTarefa)
