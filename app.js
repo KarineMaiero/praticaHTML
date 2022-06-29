8e51ffe9df213e4cbb064fd58462ddc38130fa5a
karinemaiero <karine_gino@yahoo.com.br>
2022-06-29 18:36:52 -0300

    parte 5 concluida e 6 quase completa

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ let tarefas3 = `Atualizar LinkedIn`
 
 console.log(`Minhas tarefas são: ${tarefas1}, ${tarefas2} e ${tarefas3}`)
 
+// parte 2
+
 let tarefas = []
 
 tarefas.push(tarefas1, tarefas2, tarefas3)
 
 console.log(tarefas)
+
+// parte 3
 
 let novaTarefa = `Regar plantas`
 
@@ -18,6 +22,35 @@ tarefas.push(novaTarefa)
 
 console.log(tarefas)
 
-for(i = 0; i < tarefas.length; i++) {
-    console.log(`Minha tarefa é ${tarefas[i]}`)
+//parte 4 e 5
+
+function exibirTarefas() {
+    for(i = 0; i < tarefas.length; i++) {
+        console.log(`Minha tarefa é ${tarefas[i]}`)
+    }
 }
+exibirTarefas()
+
+function adicionarTarefa(tarefa) {
+    tarefa = prompt(`Informe uma nova tarefa:`)
+    tarefas.push(tarefa)
+    console.log(tarefas)
+}
+adicionarTarefa()
+
+// parte 6
+
+const maisUmaNovaTarefa = {
+    id: 1,
+    titulo: 'Calibrar o pneu da bicicleta',
+    descricao: `Levar a bicicleta vermelha ao posto e calibrar o pneu traseiro com 32 psi`
+}
+
+let arrayTarefas = [maisUmaNovaTarefa]
+
+function exibirNovasTarefas() {
+    console.log(`Tarefa: ${maisUmaNovaTarefa.id} \nTitulo: ${maisUmaNovaTarefa.titulo} \nDescrição: ${maisUmaNovaTarefa.descricao}`)
+}
+exibirNovasTarefas()
+
+// faltam os últimos três itens da parte 6
